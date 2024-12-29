446ebcac81b96b0d53e07ded9980eec6570a7d8c
unchalante <jonathanchilongani@gmail.com>
2024-12-29 20:23:20 +0300

    initialize player command

--- 2D pong game.cpp
+++ 2D pong game.cpp
@@ -87,26 +87,22 @@ public:
     inline int getY() { return y; }
     inline void moveUp() {y--; }
     inline void moveDown() { y++; }
-        friend ostream & operator<<(ostream & o, cBall c)
+        friend ostream & operator<<(ostream & o, cPaddle c)
     {
-        o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
+        o << "Paddle [" << c.x << "," << c.y << "];
         return o;
     }
  }; 
 int main()
 {
-    cBall c(0, 0);
-    cout << c << endl;
-    c.randomDirection();
-    cout << c << endl;
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    c.Move();
-    cout << c << endl;
-    c.randomDirection();
-    c.Move();
-    cout << c << endl;
+     cPaddle p1(0, 0);
+     cPaddle p2(10, 0);
+      cout << p1 << endl;
+      cout << p2 << endl;
+      p1. moveUp();
+      p2. moveDown();
+      cout << p1 << endl;
+      cout << p2 << endl;
 
     return 0;
 }
