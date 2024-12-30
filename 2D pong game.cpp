8219d8a316106801ce6a4c85a88f41acf847ca3b
unchalante <jonathanchilongani@gmail.com>
2024-12-30 16:10:00 +0300

    initialize paddle movement

--- 2D pong game.cpp
+++ 2D pong game.cpp
@@ -87,22 +87,23 @@ public:
     inline int getY() { return y; }
     inline void moveUp() {y--; }
     inline void moveDown() { y++; }
-        friend ostream & operator<<(ostream & o, cPaddle c)
+    friend ostream & operator<<(ostream & o, cPaddle c)
     {
-        o << "Paddle [" << c.x << "," << c.y << "];
+        o << "Paddle [" << c.x << "," << c.y << "]";
         return o;
     }
- }; 
+  };
 int main()
 {
-     cPaddle p1(0, 0);
-     cPaddle p2(10, 0);
-      cout << p1 << endl;
-      cout << p2 << endl;
-      p1. moveUp();
-      p2. moveDown();
-      cout << p1 << endl;
-      cout << p2 << endl;
+    cPaddle p1(0, 0);
+    cPaddle p2(10, 0);
+    cout << p1 << endl;
+    cout << p2 << endl;
+    p1.moveUp();
+    p2.moveDown();
+    cout << p1 << endl;
+    cout << p2 << endl;
+    
 
     return 0;
 }
